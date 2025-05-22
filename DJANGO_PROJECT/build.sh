a91779165ca45609f3c3da60a29c7f87229f257a
Viverun <khanjamilahmed202@gmail.com>
2025-05-22 03:06:35 -1200

    Fix build.sh line endings and shebang for Render

--- DJANGO_PROJECT/build.sh
+++ DJANGO_PROJECT/build.sh
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash# Build script for Renderset -o errexit  # Exit on error# Initial setuppython -m pip install --upgrade pip# Install dependenciespip install -r requirements.txt# Collect static filespython manage.py collectstatic --no-input# Apply database migrationspython manage.py migrate
+#!/usr/bin/env bash
+# Build script for Render
+set -o errexit  # Exit on error
+
+# Initial setup
+python -m pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
